5a597792d502d6382fe8208b13100008eb51d34e
Ehsan <57229151+EhsanMousaviMsl@users.noreply.github.com>
2023-04-07 00:35:22 +0300

    commit all

---
+++ Qt_ws/build/CMakeFiles/SayHi2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SayHi2.dir/main.cpp.o"
+  "CMakeFiles/SayHi2.dir/main.cpp.o.d"
+  "SayHi2"
+  "SayHi2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SayHi2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
